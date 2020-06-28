3319a4d4f317320cb59cf24100dba1f59b760680
mhamzaali <39744743+mhamzaali@users.noreply.github.com>
2020-06-28 12:16:25 +0500

    increment update button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className={`${IsMorning ? 'morning' : 'night'}`}>
       <h1>The Count is : {Count}</h1> 
-      <button onClick={() => SetCount(++Count)}>Update Counter</button>
+      <button onClick={() => SetCount(++Count)}>Increment Counter</button>
       <h1>Good {IsMorning ? 'Morning' : 'Night'}</h1>
       <button onClick={ () => SetMorning(!IsMorning)}>Update Day</button>
     </div>
