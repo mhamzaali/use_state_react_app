5948c5256a07a9d84f300eb6fd502a8bb1946183
mhamzaali <39744743+mhamzaali@users.noreply.github.com>
2020-06-28 12:04:54 +0500

    update 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  let [Count,SetCount] = useState(1);
+  let [IsMorning,SetMorning] = useState(false);
+
+  return (
+    <div className={`${IsMorning ? 'morning' : 'night'}`}>
+      <h1>The Count is : {Count}</h1> 
+      <button onClick={() => SetCount(++Count)}>Update Counter</button>
+      <h1>Good {IsMorning ? 'Morning' : 'Night'}</h1>
+      <button onClick={ () => SetMorning(!IsMorning)}>Update Day</button>
+    </div>
+  );
+}
+
+export default App;
